7176d86d1d28325bde0568c8466e4314e2d8c9b4
luizm <luizmmrocha@gmail.com>
2021-12-07 14:09:59 -0300

    studies

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log(__dirname);
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+	// const text = fs.readFileSync('./content/big.txt', 'utf8')
+	// res.end(text)
+	const fileStream = fs.createReadStream('./content/big.txt', 'utf8');
+	fileStream.on('open', () => {
+		// pipe to a readable stream
+		fileStream.pipe(res);
+	});
+	fileStream.on('error', (err) => {
+		res.end(err);
+	});
+}).listen(5000);
